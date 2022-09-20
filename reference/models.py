d218a876164b2374afe5679ea6d2c5270f123b5e
Coolkat64 <arthurbartoli03@gmail.com>
2022-09-20 11:08:22 +0200

    Added Book model

--- reference/models.py
+++ reference/models.py
@@ -1,3 +1,16 @@
+from django.test import TestCase
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    
+    title = models.CharField(max_length=200)
+    og_title = models.CharField(max_length=200)
+    author = models.CharField(max_length=50)
+    desc = models.TextField()
+    # 14 chars := 13 digits + 1 dash to seperate code and checksum
+    isbn = models.CharField(max_length=14)
+    published_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
