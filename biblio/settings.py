e60f313bb43213f1e0951af733ae6d64333a6a9a
Coolkat64 <arthurbartoli03@gmail.com>
2022-09-20 10:59:10 +0200

    Removed previous model.py and created app 'reference'

--- biblio/settings.py
+++ biblio/settings.py
@@ -74,9 +74,9 @@ WSGI_APPLICATION = "biblio.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
