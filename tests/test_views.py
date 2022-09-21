98b9902bf599ee465440ce456d8581121ce08dff
Coolkat64 <arthurbartoli03@gmail.com>
2022-09-21 22:02:47 +0200

    Reformat OG test

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,10 +1,23 @@
 from django.test import TestCase
+from reference.models import Book
 
+class BookListViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        nb_books = 42
 
-class ViewsTestCase(TestCase):
-    def test_main_page_displaying_correctly(self):
+        for book_id in range(nb_books):
+            Book.objects.create(
+                title=f'Titre {book_id}',
+                og_title=f'Title {book_id}',
+                author=f'Auteur {book_id}',
+                desc=f'Desc {book_id}',
+                isbn=f'ISBN-{book_id}',
+                published_year=2020 + book_id,
+                is_lent=True
+            )
+
+    def test_view_url_exists_at_desired_location(self):
         """The main page displays correctly"""
         response = self.client.get('http://0.0.0.0:8000')
         self.assertEqual(response.status_code, 200)
-
-
