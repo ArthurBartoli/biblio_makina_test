f19d6ee27a2c8ebfb924bab3693b3e9007c79b5b
Coolkat64 <arthurbartoli03@gmail.com>
2022-09-23 13:42:06 +0200

    Docstrings to views + test to the author's list view

--- reference/views.py
+++ reference/views.py
@@ -6,24 +6,25 @@ from .forms import BookForm
 
 
 class BookView(ListView):
-    # We specify the model for list view
     model = Book
 
 def authors_list(request):
+    '''Displays a list of unique authors from the library'''
     data = Book.objects.all().values_list('author').distinct().order_by('author')
     data = [result_tuple[0] for result_tuple in data]
     return render(request, 'reference/authors_list.html', {'authors': list(data)})
 
 def books_by_author(request, author):
+    '''Displays all books by a specific author'''
     data = Book.objects.all().filter(author=author).order_by('title')
     return render(request, 'reference/book_list.html', {'object_list': data})
 
+### Add/Set views for books ###
 def book_new(request):
     if request.method == "POST":
         form = BookForm(request.POST)
         if form.is_valid():
             book = form.save(commit=False)
-            ### TODO: Include ISBN formatting
             book.save()
             return redirect('book_view')
     else:
@@ -36,7 +37,6 @@ def book_edit(request, pk):
         form = BookForm(request.POST, instance=book)
         if form.is_valid():
             book = form.save(commit=False)
-            ### TODO: Include ISBN formatting
             book.save()
             return redirect('book_view')
     else:
@@ -48,7 +48,9 @@ def book_delete(request, pk):
     book.delete()
     return redirect('book_view')
 
+
 def book_lending(resquest, pk):
+    '''Sets lending status to false if book is already lent, and reverse'''
     book = get_object_or_404(Book, pk=pk)
     if book.is_lent:
         book.is_lent = False
@@ -57,5 +59,4 @@ def book_lending(resquest, pk):
         book.is_lent = True
         book.save()
 
-    # TODO: UNIT TEST !!!
     return redirect('book_view')
--- tests/test_views.py
+++ tests/test_views.py
@@ -9,16 +9,16 @@ from reference.forms import BookForm
 class AddBookViewTest(TestCase):
 
     def setUp(self):
-            dummyData = {}
-            dummyData['title'] = "Le Test"
-            dummyData['og_title'] = "The Test"
-            dummyData['author'] = "Mister Test"
-            dummyData['desc'] = "Ceci n'est pas un test"
-            dummyData['isbn'] = "74-86-Le-Test"
-            dummyData['published_year'] = 2022
-            dummyData['is_lent'] = False
+        dummyData = {}
+        dummyData['title'] = "Le Test"
+        dummyData['og_title'] = "The Test"
+        dummyData['author'] = "Mister Test"
+        dummyData['desc'] = "Ceci n'est pas un test"
+        dummyData['isbn'] = "74-86-Le-Test"
+        dummyData['published_year'] = 2022
+        dummyData['is_lent'] = False
 
-            self.data = dummyData
+        self.data = dummyData
 
     def test_post_success(self):
         '''Is a new book correctly added to the DB ?'''
@@ -34,26 +34,26 @@ class AddBookViewTest(TestCase):
 class EditBookViewTest(TestCase):
 
     def setUp(self):
-            self.test_book = Book.objects.create(
-                    title='Titre',
-                    og_title='Title',
-                    author='Auteur',
-                    desc='Desc',
-                    isbn='ISBN',
-                    published_year=1,
-                    is_lent=True
-                )
-            
-            dummyData = {}
-            dummyData['title'] = "Le Test"
-            dummyData['og_title'] = "The Test"
-            dummyData['author'] = "Mister Test"
-            dummyData['desc'] = "Ceci n'est pas un test"
-            dummyData['isbn'] = "74-86-Le-Test"
-            dummyData['published_year'] = 2022
-            dummyData['is_lent'] = False
+        self.test_book = Book.objects.create(
+                title='Titre',
+                og_title='Title',
+                author='Auteur',
+                desc='Desc',
+                isbn='ISBN',
+                published_year=1,
+                is_lent=True
+            )
+        
+        dummyData = {}
+        dummyData['title'] = "Le Test"
+        dummyData['og_title'] = "The Test"
+        dummyData['author'] = "Mister Test"
+        dummyData['desc'] = "Ceci n'est pas un test"
+        dummyData['isbn'] = "74-86-Le-Test"
+        dummyData['published_year'] = 2022
+        dummyData['is_lent'] = False
 
-            self.data = dummyData
+        self.data = dummyData
 
     def test_edit_success(self):
         '''Is a book correctly edited in the DB ?'''
@@ -77,15 +77,15 @@ class EditBookViewTest(TestCase):
 class DeleteBookViewTest(TestCase):
 
     def setUp(self):
-            self.test_book = Book.objects.create(
-                    title='Titre',
-                    og_title='Title',
-                    author='Auteur',
-                    desc='Desc',
-                    isbn='ISBN',
-                    published_year=1,
-                    is_lent=True
-                )
+        self.test_book = Book.objects.create(
+                title='Titre',
+                og_title='Title',
+                author='Auteur',
+                desc='Desc',
+                isbn='ISBN',
+                published_year=1,
+                is_lent=True
+            )
 
     def test_delete_success(self):
         '''Is a book correctly deleted from the DB ?'''
@@ -101,25 +101,25 @@ class DeleteBookViewTest(TestCase):
 class LendingBookViewTest(TestCase):
 
     def setUp(self):
-            self.test_book_lent = Book.objects.create(
-                    title='Titre',
-                    og_title='Title',
-                    author='Auteur',
-                    desc='Desc',
-                    isbn='ISBN',
-                    published_year=1,
-                    is_lent=True
-                )
-            
-            self.test_book_not_lent = Book.objects.create(
-                    title='Titre',
-                    og_title='Title',
-                    author='Auteur',
-                    desc='Desc',
-                    isbn='ISBN',
-                    published_year=1,
-                    is_lent=False
-                )
+        self.test_book_lent = Book.objects.create(
+                title='Titre',
+                og_title='Title',
+                author='Auteur',
+                desc='Desc',
+                isbn='ISBN',
+                published_year=1,
+                is_lent=True
+            )
+        
+        self.test_book_not_lent = Book.objects.create(
+                title='Titre',
+                og_title='Title',
+                author='Auteur',
+                desc='Desc',
+                isbn='ISBN',
+                published_year=1,
+                is_lent=False
+            )
 
     def test_unlending_success(self):
         '''Is a book's lending status correctly changed to false from the DB ?'''
@@ -145,6 +145,36 @@ class LendingBookViewTest(TestCase):
         new_book = Book.objects.get(pk=self.test_book_not_lent.pk)
         self.assertTrue(getattr(new_book, 'is_lent'))
 
+class AuthorsListViewTest(TestCase):
+
+    def setUp(self):
+        self.data = []
+
+        authors = ['Bob', 'Alice', 'Zeitoun', 'Alice', 'Dan']
+
+        for author in authors:
+            self.data = Book.objects.create(
+                title='Titre',
+                og_title='Title',
+                author=author,
+                desc='Desc',
+                isbn='ISBN',
+                published_year=1,
+                is_lent=False
+            )
+
+    def test_display_success(self):
+        '''Is the list of unique authors displayed as intended ? 
+        '''
+
+        response = self.client.get(reverse('authors_list'))
+
+        authors = ['Alice', 'Bob', 'Dan', 'Zeitoun']
+        for author in authors:
+            self.assertContains(response, author)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        
 
 
 class ViewResponseTest(TestCase):
@@ -174,8 +204,16 @@ class ViewResponseTest(TestCase):
     def test_view_response_new_book(self):
         """The new book page displays correctly"""
         response = self.client.get(reverse('book_new'))
-
         self.assertEqual(response.status_code, HTTPStatus.OK)
+    def test_view_response_author_list(self):
+        """The lending set book page displays correctly"""
+        response = self.client.get(reverse('authors_list'))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_view_response_books_by_author(self):
+        response = self.client.get(reverse('books_by_author', args=[self.test_books[0].author]))
+        self.assertEqual(response.status_code, HTTPStatus.OK) 
+    
 
     def test_view_response_edit_book(self):
         """The book edit page displays correctly"""
@@ -191,3 +229,5 @@ class ViewResponseTest(TestCase):
         response = self.client.get(reverse('book_lending', args=[self.test_books[2].pk]))
         self.assertEqual(response.status_code, HTTPStatus.FOUND) # The user is redirected..
         self.assertEqual(response["Location"], reverse('book_view')) # ..to main page
+    
+    
