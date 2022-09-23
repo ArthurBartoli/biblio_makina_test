5c162f4c63e64a8bc0d53665b481264e749d9f80
Coolkat64 <arthurbartoli03@gmail.com>
2022-09-23 14:43:04 +0200

    Debug to False

--- biblio/settings.py
+++ biblio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8c=$1mhqps12yuj+83fvp=(u59v%=$(lytclo7a5i8o2v-qh@&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
